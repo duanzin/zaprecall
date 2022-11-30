ae641f97712a7c86da178e62c44aa6342c0e22c0
duanzin <lucaduan@gmail.com>
2022-11-30 20:35:35 -0300

    criação dos arquivos js e layout vazio iniciado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyle from "./globalStyle";
+import styled from "styled-components";
+import logo from "./assets/img/logo.png";
+import Deck from "./Deck";
+import Contador from "./Contador";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Main>
+        <Header>
+          <img src={logo} /> <h1>ZapRecall</h1>
+        </Header>
+        <Deck />
+        <Contador />
+      </Main>
+    </>
   );
 }
+
+const Main = styled.div`
+  background-color: #fb6b6b;
+  width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0px;
+  padding: 0px;
+  padding-bottom: 200px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 40px 0 20px 0;
+  img {
+    width: 52px;
+  }
+  h1 {
+    font-family: "Righteous";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 36px;
+    line-height: 45px;
+    color: #ffffff;
+    margin-left: 20px;
+  }
+`;
 
 export default App;
